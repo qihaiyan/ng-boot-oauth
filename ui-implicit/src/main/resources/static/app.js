a6911e626b2b1f95876079d2cd62b20f55062c16
qihaiyan <haiyan_qi@hotmail.com>
2016-10-06 23:53:25 +0800

    Using satellizer to implement oauth2 implict flow

---
+++ ui-implicit/src/main/resources/static/app.js
@@ -0,0 +1,75 @@
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', 'ui.router', 'satellizer'])
+  .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+
+    /**
+     * Helper auth functions
+     */
+    var skipIfLoggedIn = ['$q', '$auth', function($q, $auth) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.reject();
+      } else {
+        deferred.resolve();
+      }
+      return deferred.promise;
+    }];
+
+    var loginRequired = ['$q', '$location', '$auth', function($q, $location, $auth) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.resolve();
+      } else {
+        $location.path('/login');
+      }
+      return deferred.promise;
+    }];
+
+    /**
+     * App routes
+     */
+    $stateProvider
+      .state('home', {
+        url: '/',
+        controller: 'HomeCtrl',
+        templateUrl: 'partials/home.html'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl',
+        resolve: {
+          skipIfLoggedIn: skipIfLoggedIn
+        }
+      })
+      .state('logout', {
+        url: '/logout',
+        template: null,
+        controller: 'LogoutCtrl'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'partials/profile.html',
+        controller: 'ProfileCtrl',
+        resolve: {
+          loginRequired: loginRequired
+        }
+      });
+//    $urlRouterProvider.otherwise('/');
+
+    /**
+     *  Satellizer config
+     */
+    $authProvider.github({
+      clientId: '91979aad542dfbe50b68',
+      responseType: 'token'
+    });
+
+    $authProvider.oauth2({
+      name: 'authserver',
+      url: 'http://localhost:9999/uaa',
+      clientId: 'acme',
+      responseType: 'token',
+      redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+      authorizationEndpoint: 'http://localhost:9999/uaa/oauth/authorize'
+    });
+  });
